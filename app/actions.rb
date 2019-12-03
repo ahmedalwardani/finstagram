aec67756398ba22ae819ab197fa9cc9912252864
Ahmed Alwardani <ahmedalwardani@MacBook-Pro-de-Ahmed.local>
2019-12-02 20:49:35 -0500

    created classes and dynamic

--- app/actions.rb
+++ app/actions.rb
@@ -9,56 +9,56 @@
 #     erb(:index)
 # end
 
- def get_humanized_time_ago (time_ago_in_minutes)
-    if time_ago_in_minutes >= 60
-        "#{time_ago_in_minutes/60} hours ago"
+#  def get_humanized_time_ago (time_ago_in_minutes)
+#     if time_ago_in_minutes >= 60
+#         "#{time_ago_in_minutes/60} hours ago"
 
-    else
-        "#{time_ago_in_minutes} minutes ago"
-    end
-end
-# get '/' do
-#   "Hello world!"
+#     else
+#         "#{time_ago_in_minutes} minutes ago"
+#     end
 # end
+# # get '/' do
+# #   "Hello world!"
+# # end
 
-get '/' do
-    @finstagram_post_shark = {
-    username: "sharky_j",
-    avatar_url: "http://naserca.com/images/sharky_j.jpg",
-    photo_url: "http://naserca.com/images/shark.jpg",
-    get_humanized_time_ago: get_humanized_time_ago(15),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-        username: "sharky_j",
-        text:"Out for the long weekend... too embarrassed to show y'all the beach body"
-    }]
-    }
+# get '/' do
+#     @finstagram_post_shark = {
+#     username: "sharky_j",
+#     avatar_url: "http://naserca.com/images/sharky_j.jpg",
+#     photo_url: "http://naserca.com/images/shark.jpg",
+#     get_humanized_time_ago: get_humanized_time_ago(15),
+#     like_count: 0,
+#     comment_count: 1,
+#     comments: [{
+#         username: "sharky_j",
+#         text:"Out for the long weekend... too embarrassed to show y'all the beach body"
+#     }]
+#     }
 
-  @finstagram_post_whale = {
-    username: "kirk_whalum",
-    avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-    photo_url: "http://naserca.com/images/whale.jpg",
-    get_humanized_time_ago: get_humanized_time_ago(65),
-    like_count: 0,
-    comment_count: 1,
-    comments: [{
-        username: "kirk_whalum",
-        text: "#weekendvibes"
-    }]
-    }
+#   @finstagram_post_whale = {
+#     username: "kirk_whalum",
+#     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+#     photo_url: "http://naserca.com/images/whale.jpg",
+#     get_humanized_time_ago: get_humanized_time_ago(65),
+#     like_count: 0,
+#     comment_count: 1,
+#     comments: [{
+#         username: "kirk_whalum",
+#         text: "#weekendvibes"
+#     }]
+#     }
 
-  @finstagram_post_marlin = {
-    username: "marlin_peppa",
-    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-    photo_url: "http://naserca.com/images/marlin.jpg",
-    get_humanized_time_ago: get_humanized_time_ago(190),
-    like_count: 1,
-    comments: [{
-        username: "marlin_peppa",
-        text: "Shawerma Palace time! ;)"
-    }]
-    }
+#   @finstagram_post_marlin = {
+#     username: "marlin_peppa",
+#     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+#     photo_url: "http://naserca.com/images/marlin.jpg",
+#     get_humanized_time_ago: get_humanized_time_ago(190),
+#     like_count: 1,
+#     comments: [{
+#         username: "marlin_peppa",
+#         text: "Shawerma Palace time! ;)"
+#     }]
+#     }
 
     # [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin].to_s
     
@@ -76,7 +76,10 @@ get '/' do
 #     end
 # end
 
-@finstagram_posts=[@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+# @finstagram_posts=[@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
 
+
+get '/' do
+    @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
 end
